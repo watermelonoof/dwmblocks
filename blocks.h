cd0162795fd99dd5f562dde6e1333f6641164c77
Bryan Li <bryanli1211@gmail.com>
2023-07-03 14:11:50 +0800

    custom patch

--- blocks.h
+++ blocks.h
@@ -3,11 +3,12 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", 	"sb-nettraf",	1,	0},
 	{"", 	"sb-cpu",	1,	1},
-	{"", 	"sb-memory",	1,	2},
+	{"", 	"sb-volume",	1,	2},
 	/* {"", 	"sb-disk",	60,	3}, */
-	{"", 	"sb-battery",	30,	4},
+	{"", 	"sb-battery",	1,	4},
 	/* {"", 	"sb-internet",	5,	5}, */
 	{"",	"sb-clock",	1,	6},
+	{"",	"sb-internet",	30,	7},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
