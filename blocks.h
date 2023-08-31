f14109b2fe583128cb786191d794c75a33a76a8c
Bryan Li <bryanli1211@gmail.com>
2023-09-01 07:10:54 +0800

    applied patch and adjusted blocks

--- blocks.h
+++ blocks.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", 	"sb-nettraf",	1,	0},
-	/* {"", 	"sb-cpu",	1,	1}, */
-	{"", 	"sb-volume",	60,	10},
-	/* {"", 	"sb-disk",	60,	3}, */
+	/* {"", 	"sb-nettraf",	1,	2}, */
+	{"", 	"sb-cpu",		1,	1},
+	{"", 	"sb-volume",	60,	5},
+	{"", 	"sb-disk",		60,	3},
 	{"", 	"sb-battery",	5,	4},
 	{"",	"sb-internet",	30,	7},
 	{"",	"sb-clock",		1,	6},
